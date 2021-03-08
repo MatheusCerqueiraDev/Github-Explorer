aa3662b1402c78d462ac35cff2118133bfa22d6a
Matheus Cerqueira <matheuscerqueira12@icloud.com>
2021-03-08 11:05:49 -0300

    Adicionando e configurando Babel

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,4 +1,9 @@
-const user = {
-  name: 'Matheus'
-};
-console.log(user.adress?.street);
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function App() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World");
+}
