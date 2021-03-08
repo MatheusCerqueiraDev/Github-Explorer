7cbdc9473366f3dccd8ff37a6d709a840ab4ab1e
Matheus Cerqueira <matheuscerqueira12@icloud.com>
2021-03-08 14:28:56 -0300

    Aula 3

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,5 @@
-   import React from 'react';
-   import { App } from './App'; //para importarmos uma função usamos chaves com o nome da mesma dentro
+import { render } from 'react-dom';
+import { App } from './App'; //para importarmos uma função usamos chaves com o nome da mesma dentro
+
+   render(<App/>,document.getElementById('root'))//render é um função de renderização
+//dentro das aspas recebe-se dois paramentros, 1° o que quer reinderizar e o 2° aonde eu quero reinderizar
